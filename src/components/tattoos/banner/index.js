48da86e5f251650b4a35d6dd5986f7654614a969
pRay01 <pray9563@gmail.com>
2022-03-19 19:04:01 +0530

    done

--- src/components/tattoos/banner/index.js
+++ src/components/tattoos/banner/index.js
@@ -6,7 +6,7 @@ function Banner(){
         <div className="  lg:pl-8 text-center lg:text-left w-full lg:w-6/12">
          <div className='lg:p-6 lg:pt-20'>
            <h2 className='text-2xl lg:text-5xl 2xl:text-8xl font-bold pt-4 tracking-widest' >DIMENSIONLESS TATTOOS</h2>
-           <h5 className='pt-2 lg:pt-10 lg:text-lg 2xl:text-4xl 2xl:pt-20 text-[#343434] montserrat'>IMAGINE-DESIGN-TATTOO</h5>
+           <h5 className='pt-2 lg:pt-10 lg:text-lg 2xl:text-4xl 2xl:pt-20 text-[#343434] dark:text-white montserrat'>IMAGINE-DESIGN-TATTOO</h5>
            <div className=' py-10 lg:py-16 2xl:pt-40'>
            <button className='dark:text-black button-shadow rounded-lg 2xl:rounded-2xl 2xl:px-10 2xl:py-4 2xl:text-3xl  dark:bg-[#8AB4F8] bg-[#1A73E8] text-[#FFFFFF] text-sm montserrat py-1 px-8'>Book a Free Consulation</button>
            </div>
