0338979a6f7b65bb52a35e29316e71c42c9276fb
pRay01 <pray9563@gmail.com>
2022-03-19 14:56:13 +0530

    about done

--- src/components/about/banner/index.js
+++ src/components/about/banner/index.js
@@ -1,20 +1,20 @@
 import about from '../../../assets/Group 1755.svg'
 function Banner(){
     return(
-        <section className='dark:bg-[#202124] dark:text-white p-6 min-h-screen lg:px-36'>
+        <section className='dark:bg-[#202124] dark:text-white p-6 min-h-80 2xl:pt-28 lg:px-36'>
             <div className=' dark:bg-[#202124] dark:text-white bg-white pt-10 pb-10 flex flex-wrap items-center '> 
-               <div className=' w-full  lg:w-4/12 '>
-                   <div className=' font-semibold text-2xl lg:text-4xl text-center lg:text-left lg:pl-10  '>
+               <div className='lg:pl-14 w-full  lg:w-5/12 '>
+                   <div className=' font-bold text-2xl lg:text-4xl 2xl:text-7xl text-center lg:text-left lg:pl-10  '>
                        <h2>About Us</h2>
                    </div>
-                   <p className='dark:text-[#B5B5B5] montserrat px-4 lg:px-0 lg:pl-9 pt-4 text-[#141010] text-center lg:text-left'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed d</p>
-                  <div className='flex justify-center lg:justify-start py-12 lg:px-9'>
-                  <button className='dark:bg-[#8AB4F8] dark:text-black  button-shadow rounded-lg bg-[#1A73E8] text-[#FFFFFF] px-10 py-1 text-sm montserrat'>Call Us Now</button>    
+                   <p className='dark:text-[#B5B5B5] montserrat px-4 lg:px-0 lg:pl-9 pt-4 lg:pt-6 2xl:pt-10 text-[#141010] text-center lg:text-xl 2xl:text-3xl lg:text-left'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed d</p>
+                  <div className='flex justify-center lg:justify-start py-12 lg:pt-14 2xl:pt-36 lg:px-9'>
+                  <button className='dark:bg-[#8AB4F8] dark:text-black  button-shadow rounded-lg bg-[#1A73E8] text-[#FFFFFF] px-10 py-1 text-sm montserrat lg:px-14 lg:py-2 lg:rounded-xl 2xl:px-24 2xl:py-3 2xl:text-2xl 2xl:rounded-2xl'>Call Us Now</button>    
                   </div>           
                </div>
-               <div className=' pt-10 lg:pt-0 flex justify-center lg:justify-end px-10 lg:px-0 lg:w-8/12'>
-                   <div className=''>
-                       <img  src={about}/>
+               <div className=' pt-10 lg:pt-0 flex justify-center lg:justify-end px-10 lg:px-0 lg:w-7/12'>
+                   <div className='w-full'>
+                       <img  className='w-full' src={about}/>
                    </div>
                </div>
           </div>
