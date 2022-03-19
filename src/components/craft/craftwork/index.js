9cc56500f2b99c3455fb2a4d1f1dd42ec82225b5
pRay01 <pray9563@gmail.com>
2022-03-19 12:30:33 +0530

    craft page done

--- src/components/craft/craftwork/index.js
+++ src/components/craft/craftwork/index.js
@@ -3,14 +3,14 @@ import{BiRupee} from 'react-icons/bi'
 function Craftwork(){
     return(
         <section className="dark:bg-[#202124]  dark:text-white p-6 lg:px-40">
-        <div  className='dark:bg-[#202124]  dark:text-white  flex flex-wrap justify-center text-center items-center'>
-             <h2 className='font-semibold text-3xl'>CRAFTWORK</h2>
+        <div  className='dark:bg-[#202124] lg:pb-8 2xl:pt-10 2xl:pb-14  dark:text-white  flex flex-wrap justify-center text-center items-center'>
+             <h2 className='font-semibold text-3xl 2xl:text-5xl'>CRAFTWORK</h2>
         </div>
         <div className='dark:bg-[#202124]   pt-4 w-full flex flex-wrap justify-center lg:justify-between items-center text-center '>
               <div className="  w-8/12 lg:w-3/12 mt-4 lg:mt-0 text-center  rounded-xl overflow-hidden ">
                       <div><img  src={art2}/>
-                      <div className='darkmode bg-[#EEEEEE] items-center flex justify-evenly '>
-                      <div className='darkmode bg-[#EEEEEE] flex montserrat justify-center items-center'>
+                      <div className='darkmode 2xl:py-4 2xl:text-2xl bg-[#EEEEEE] items-center flex justify-evenly '>
+                      <div className='darkmode  bg-[#EEEEEE] flex montserrat justify-center items-center'>
                             <div><BiRupee/></div>
                             <p className="darkmode bg-[#EEEEEE] p-2 text-black">1000</p>
                     </div>
@@ -20,7 +20,7 @@ function Craftwork(){
                </div> 
               <div className="  w-8/12 lg:w-3/12 mt-6 lg:mt-0 text-center  rounded-xl overflow-hidden ">
                       <div><img  src={art2}/>
-                      <div className='darkmode bg-[#EEEEEE] items-center flex justify-evenly '>
+                      <div className='darkmode 2xl:py-4 2xl:text-2xl bg-[#EEEEEE] items-center flex justify-evenly '>
                       <div className='darkmode bg-[#EEEEEE] flex montserrat justify-center items-center'>
                             <div><BiRupee/></div>
                             <p className="darkmode bg-[#EEEEEE] p-2 text-black">1000</p>
@@ -31,7 +31,7 @@ function Craftwork(){
                </div> 
                <div className="  w-8/12 lg:w-3/12 mt-6 lg:mt-0 text-center  rounded-xl overflow-hidden ">
                       <div><img  src={art2}/>
-                      <div className='darkmode bg-[#EEEEEE] items-center flex justify-evenly '>
+                      <div className='darkmode 2xl:py-4 2xl:text-2xl bg-[#EEEEEE] items-center flex justify-evenly '>
                       <div className='darkmode bg-[#EEEEEE] flex montserrat justify-center items-center'>
                             <div><BiRupee/></div>
                             <p className="darkmode bg-[#EEEEEE] p-2 text-black">1000</p>
@@ -41,10 +41,10 @@ function Craftwork(){
                        </div>
                </div> 
         </div>
-        <div className=' pt-4 w-full flex flex-wrap justify-center lg:justify-between items-center text-center '>
+        <div className=' pt-4 2xl:pt-10 w-full flex flex-wrap justify-center lg:justify-between items-center text-center '>
               <div className="  w-7/12 lg:w-3/12 mt-4 lg:mt-0 text-center  rounded-xl overflow-hidden ">
                       <div><img  src={art2}/>
-                      <div className='darkmode bg-[#EEEEEE] items-center flex justify-evenly '>
+                      <div className='darkmode 2xl:py-4 2xl:text-2xl bg-[#EEEEEE] items-center flex justify-evenly '>
                          <div className='darkmode bg-[#EEEEEE] flex montserrat justify-center items-center'>
                             <div><BiRupee/></div>
                             <p className="darkmode bg-[#EEEEEE] p-2 text-black">1000</p>
@@ -55,7 +55,7 @@ function Craftwork(){
                </div> 
               <div className="  w-8/12 lg:w-3/12 mt-6 lg:mt-0 text-center  rounded-xl overflow-hidden ">
                       <div><img  src={art2}/>
-                      <div className='darkmode bg-[#EEEEEE] items-center flex justify-between '>
+                      <div className='darkmode 2xl:py-4 2xl:text-2xl bg-[#EEEEEE] items-center flex justify-evenly '>
                          <div className='darkmode bg-[#EEEEEE] flex montserrat justify-center items-center'>
                             <div><BiRupee/></div>
                             <p className="darkmode bg-[#EEEEEE] p-2 text-black">1000</p>
@@ -66,7 +66,7 @@ function Craftwork(){
                </div> 
                <div className="  w-8/12 lg:w-3/12 mt-6 lg:mt-0 text-center  rounded-xl overflow-hidden ">
                       <div><img  src={art2}/>
-                      <div className='darkmode bg-[#EEEEEE] items-center flex justify-between '>
+                      <div className='darkmode 2xl:py-4 2xl:text-2xl bg-[#EEEEEE] items-center flex justify-evenly '>
                          <div className='darkmode bg-[#EEEEEE] flex montserrat justify-center items-center'>
                             <div><BiRupee/></div>
                             <p className=" darkmode bg-[#EEEEEE] p-2 text-black">1000</p>
@@ -76,10 +76,10 @@ function Craftwork(){
                        </div>
                </div> 
         </div>
-        <div className=' pt-4 w-full flex flex-wrap justify-center lg:justify-between items-center text-center '>
+        <div className=' pt-4 2xl:pt-10  w-full flex flex-wrap justify-center lg:justify-between items-center text-center '>
               <div className="  w-8/12 lg:w-3/12 mt-4 lg:mt-0 text-center  rounded-xl overflow-hidden ">
                       <div><img  src={art2}/>
-                      <div className=' darkmode bg-[#EEEEEE] items-center flex justify-between '>
+                      <div className=' darkmode 2xl:py-4 2xl:text-2xl bg-[#EEEEEE] items-center flex justify-evenly '>
                          <div className='darkmode bg-[#EEEEEE] flex montserrat justify-center items-center'>
                             <div><BiRupee/></div>
                             <p className="darkmode bg-[#EEEEEE] p-2 text-black">1000</p>
@@ -90,7 +90,7 @@ function Craftwork(){
                </div> 
               <div className="  w-8/12 lg:w-3/12 mt-6 lg:mt-0 text-center  rounded-xl overflow-hidden ">
                       <div><img  src={art2}/>
-                      <div className='darkmode bg-[#EEEEEE] items-center flex justify-between '>
+                      <div className='darkmode 2xl:py-4 2xl:text-2xl bg-[#EEEEEE] items-center flex justify-evenly '>
                          <div className='darkmode bg-[#EEEEEE] flex montserrat justify-center items-center'>
                             <div><BiRupee/></div>
                             <p className="darkmode bg-[#EEEEEE] p-2 text-black">1000</p>
@@ -101,7 +101,7 @@ function Craftwork(){
                </div> 
                <div className="  w-8/12 lg:w-3/12 mt-6 lg:mt-0 text-center  rounded-xl overflow-hidden ">
                       <div><img  src={art2}/>
-                      <div className='darkmode bg-[#EEEEEE] items-center flex justify-between '>
+                      <div className='darkmode 2xl:py-4 2xl:text-2xl bg-[#EEEEEE] items-center flex justify-evenly '>
                          <div className='darkmode bg-[#EEEEEE] flex montserrat justify-center items-center'>
                             <div><BiRupee/></div>
                             <p className="darkmode bg-[#EEEEEE] p-2 text-black">1000</p>
@@ -111,10 +111,10 @@ function Craftwork(){
                        </div>
                </div> 
         </div>
-        <div className=' pt-4 w-full flex flex-wrap justify-center lg:justify-between items-center text-center '>
+        <div className=' pt-4 2xl:pt-10  w-full flex flex-wrap justify-center lg:justify-between items-center text-center '>
               <div className="  w-8/12 lg:w-3/12 mt-4 lg:mt-0 text-center  rounded-xl overflow-hidden ">
                       <div><img  src={art2}/>
-                      <div className='darkmode bg-[#EEEEEE] items-center flex justify-between '>
+                      <div className='darkmode 2xl:py-4 2xl:text-2xl bg-[#EEEEEE] items-center flex justify-evenly '>
                          <div className='darkmode bg-[#EEEEEE] flex montserrat justify-center items-center'>
                             <div><BiRupee/></div>
                             <p className="darkmode bg-[#EEEEEE] p-2 text-black">1000</p>
@@ -125,7 +125,7 @@ function Craftwork(){
                </div> 
               <div className="  w-8/12 lg:w-3/12 mt-6 lg:mt-0 text-center  rounded-xl overflow-hidden ">
                       <div><img  src={art2}/>
-                      <div className='darkmode bg-[#EEEEEE] items-center flex justify-between '>
+                      <div className='darkmode 2xl:py-4 2xl:text-2xl bg-[#EEEEEE] items-center flex justify-evenly '>
                          <div className='darkmode bg-[#EEEEEE] flex montserrat justify-center items-center'>
                             <div><BiRupee/></div>
                             <p className="darkmode bg-[#EEEEEE] p-2 text-black">1000</p>
@@ -136,7 +136,7 @@ function Craftwork(){
                </div> 
                <div className="  w-8/12 lg:w-3/12 mt-6 lg:mt-0 text-center  rounded-xl overflow-hidden ">
                       <div><img  src={art2}/>
-                      <div className='darkmode bg-[#EEEEEE] items-center flex justify-between '>
+                      <div className='darkmode 2xl:py-4 2xl:text-2xl bg-[#EEEEEE] items-center flex justify-evenly '>
                          <div className='darkmode bg-[#EEEEEE] flex montserrat justify-center items-center'>
                             <div><BiRupee/></div>
                             <p className="darkmode bg-[#EEEEEE] p-2 text-black">1000</p>
--- src/components/craft/order/index.js
+++ src/components/craft/order/index.js
@@ -1,42 +1,60 @@
+import formimg from "../../../assets/Group 1759.svg";
+function Form() {
+  return (
+    <section className="darkmode bg-[#EEEEEE] h-screen pb-10 lg:pb-16 2xl:pb-24 p-6 lg:px-40">
+      <div className="darkmode bg-[#EEEEEE]  h-full flex justify-center flex-wrap">
+        <div className="flex justify-center lg:pt-20 2xl:pt-36 w-8/12">
+          <div className="w-full">
+            <img className="w-10/12 h-full object-cover" src={formimg} />
+          </div>
+        </div>
+        <div className="  w-full h-full lg:w-4/12 flex justify-between flex-col">
+          <h4 className="text-xl text-center lg:text-4xl 2xl:text-5xl font-semibold  py-6">Order Now</h4>
+          <div className="dark:bg-[#202124] bg-white  w-full h-70vh py-8 2xl:py-20 lg:rounded-2xl  2xl:rounded-3xl px-8 flex flex-col justify-between">
+            <div>
+            <div className="pb-4 2xl:pb-8 2xl:pl-8  ">
+              <input
+                className="dark:bg-[#464646] rounded-lg 2xl:rounded-2xl bg-[#EEEEEE] py-1 px-2 lg:px-8 2xl:py-6 2xl:px-28 "
+                placeholder="Name"
+                type="text"
+                id="name"
+              />
+            </div>
+            <div className="pb-4  2xl:pb-8 2xl:pl-8 ">
+              <input
+                className="dark:bg-[#464646] 2xl:rounded-2xl rounded-lg bg-[#EEEEEE] py-1 px-2 lg:px-8 2xl:py-6 2xl:px-28"
+                placeholder="E-mail Address"
+                type="email"
+                id="e-mail"
+              />
+            </div>
+            <div className="pb-4  2xl:pb-8 2xl:pl-8">
+              <input
+                className="dark:bg-[#464646] 2xl:rounded-2xl  rounded-lg bg-[#EEEEEE]  py-1 px-2 lg:px-8 2xl:py-6 2xl:px-28"
+                placeholder="Contact Number"
+                type="text"
+                id="number"
+              />
+            </div>
+            <div className="pb-20  2xl:pb-8 2xl:pl-8 ">
+              <input
+                className="dark:bg-[#464646] 2xl:rounded-2xl rounded-lg bg-[#EEEEEE] py-8 px-2 lg:px-8 2xl:py-6 2xl:px-28 "
+                placeholder="Get a Fresh Art ready from our Skilled ArtistS"
+                type="textbox"
+                id="date"
+              />
+            </div>
 
-
-import formimg from '../../../assets/Group 1759.svg'
-function Form(){
-    return(
-        <section className='darkmode bg-[#EEEEEE] p-6 lg:px-40'>
-             <div className='darkmode dark:text-white bg-[#EEEEEE] text-center lg:text-right font-semibold pt-6 lg:px-20 text-2xl lg:text-3xl '>
-                         <h4>
-                            Order Now
-                         </h4>
-                         </div>
-             <div className="darkmode bg-[#EEEEEE] flex justify-center items-center pt-10 flex-wrap">
-                 <div className="flex justify-center w-8/12">
-                         <div className='lg:w-4/6'> 
-                         <img src={formimg} />
-                         </div>
-                 </div>
-                 <div className=' pb-10 flex justify-center w-full lg:w-4/12'>
-                     <div className='dark:bg-[#202124] bg-white py-8 rounded-2xl px-8'>
-                       <div className='pb-4'>
-                           <input className='dark:bg-[#464646]  rounded-lg bg-[#EEEEEE] py-1 px-2 ' placeholder='Name' type="text" id="name"/>
-                       </div>
-                       <div className='pb-4'>
-                           <input  className='dark:bg-[#464646]  rounded-lg bg-[#EEEEEE] py-1 px-2'  placeholder='E-mail Address'   type="email" id="e-mail"/>
-                       </div>
-                       <div className='pb-4'>
-                           <input  className='dark:bg-[#464646]  rounded-lg bg-[#EEEEEE]  py-1 px-2' placeholder='Contact Number' type="text" id="number"/>
-                       </div>
-                       <div className='pb-20'>
-                           <input className='dark:bg-[#464646]  rounded-lg bg-[#EEEEEE] py-8 px-2 ' placeholder='Get a Fresh Art ready from our Skilled ArtistS' type="textbox" id="date"/>
-                       </div>
-                      
-                       <div className='flex justify-center  pt-4 items-center'>
-                           <button className='dark:bg-[#8AB4F8] dark:text-black bg-[#1A73E8] shadow montserrat text-white rounded-lg px-8 py-1'>Submit</button>
-                      </div> 
-                   </div>   
-                 </div>
-             </div>
-        </section>
-    )
+            </div>
+            <div className="flex justify-center  pt-4 items-center">
+              <button className="dark:bg-[#8AB4F8] dark:text-black bg-[#1A73E8] shadow montserrat text-white rounded-lg px-8 py-1 lg:px-14 2xl:px-20 2xl:py-4 2xl:rounded-2xl 2xl:text-2xl ">
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
-export default Form
+export default Form;
